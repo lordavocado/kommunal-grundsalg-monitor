8493055753cf069b6aec3db05360f74e25ed7a7d
Nichlas <44499401+lordavocado@users.noreply.github.com>
2026-01-20 21:57:12 +0100

    Create sheets.py

---
+++ .github/workflows/sheets.py
@@ -0,0 +1,12 @@
+import os
+import requests
+
+BASE = os.environ["SHEETS_WEBAPP_URL"]
+
+def append_row(sheet_name: str, row: list):
+    payload = {
+        "sheet": sheet_name,
+        "row": row
+    }
+    r = requests.post(BASE, json=payload, timeout=30)
+    r.raise_for_status()
